afcf23ab3a3608634c5eae0558a569f43f110038
delma <sdy2856@gmail.com>
2020-05-04 22:30:38 +0900

    [#2] feat :rocket: GithubLoginViewController 생성

---
+++ iOS/BaseballGame/BaseballGame/ViewControllers/GithubLoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  GithubLoginViewController.swift
+//  BaseballGame
+//
+//  Created by delma on 2020/05/04.
+//  Copyright © 2020 delma. All rights reserved.
+//
+
+import UIKit
+
+class GithubLoginViewController: UIViewController {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
