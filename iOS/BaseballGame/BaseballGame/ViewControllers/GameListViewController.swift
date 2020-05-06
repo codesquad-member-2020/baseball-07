067cc3b53e508086a082eccd11be968af81fd12e
delma <sdy2856@gmail.com>
2020-05-06 16:19:19 +0900

    [#4] feat :rocket: GameListViewController의 서브뷰들의 컨스트레인트 설정 추가

--- iOS/BaseballGame/BaseballGame/ViewControllers/GameListViewController.swift
+++ iOS/BaseballGame/BaseballGame/ViewControllers/GameListViewController.swift
@@ -9,13 +9,25 @@
 import UIKit
 
 final class GameListViewController: UIViewController {
-
+    
+    private let gameListTitle = GameListTitle()
     private let gameListCollectionView = GameListCollectionView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        configureConstraints()
     }
     
-
+    private func configureConstraints() {
+        let constraints = [
+            gameListTitle.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            gameListTitle.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 30),
+            
+            gameListCollectionView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            gameListCollectionView.topAnchor.constraint(equalTo: gameListTitle.bottomAnchor, constant: 20),
+            gameListCollectionView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -20),
+        ]
+        constraints.forEach { $0.isActive = true }
+    }
+    
 }
