8076d99ec03a6d4de4fdfe9fc748d42e796c5426
delma <sdy2856@gmail.com>
2020-05-06 12:03:31 +0900

    [#4] feat :rocket: GameListCardView 에 서브뷰 추가 및 컨스트레인트 추가

--- iOS/BaseballGame/BaseballGame/Views/Components/GameListCardView.swift
+++ iOS/BaseballGame/BaseballGame/Views/Components/GameListCardView.swift
@@ -9,13 +9,16 @@
 import UIKit
 
 class GameListCardView: UIView {
-
+    
     private let homeTeam = ParticipantLabel()
     private let visitingTeam = ParticipantLabel()
+    private let versus = VersusLabel()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         configureBackgroundColor()
+        addSubViews()
+        configureConstraints()
     }
     
     required init?(coder: NSCoder) {
@@ -26,8 +29,27 @@ class GameListCardView: UIView {
         self.backgroundColor = #colorLiteral(red: 1, green: 0.730245653, blue: 0, alpha: 1)
     }
     
+    private func addSubViews() {
+        self.addSubview(homeTeam)
+        self.addSubview(versus)
+        self.addSubview(visitingTeam)
+    }
+    
     private func configureConstraints() {
+        let constraints = [
+            versus.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            versus.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            
+            visitingTeam.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            visitingTeam.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: self.frame.width/10),
+            visitingTeam.trailingAnchor.constraint(equalTo: versus.leadingAnchor, constant: self.frame.width/10),
+            
+            homeTeam.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            homeTeam.leadingAnchor.constraint(equalTo: versus.trailingAnchor, constant: self.frame.width/10),
+            homeTeam.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: self.frame.width/10),
+        ]
         
+        constraints.forEach { $0.isActive = true }
     }
     
 }
