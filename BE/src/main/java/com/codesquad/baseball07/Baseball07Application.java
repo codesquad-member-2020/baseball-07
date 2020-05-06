0158e3f501efce90b32fe1c633ac8905d52c044e
koo bohyeon <goosee115@gmail.com>
2020-05-06 16:48:25 +0900

    feat:rocket: 프로젝트 세팅

---
+++ BE/src/main/java/com/codesquad/baseball07/Baseball07Application.java
@@ -0,0 +1,13 @@
+package com.codesquad.baseball07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Baseball07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Baseball07Application.class, args);
+	}
+
+}
