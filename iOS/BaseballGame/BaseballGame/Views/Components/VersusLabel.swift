fbb604ae77d3048145f338f7a325026d65cae88f
delma <sdy2856@gmail.com>
2020-05-06 11:49:23 +0900

    [#4] feat :rocket: versus label 추가

---
+++ iOS/BaseballGame/BaseballGame/Views/Components/VersusLabel.swift
@@ -0,0 +1,29 @@
+//
+//  VersusLabel.swift
+//  BaseballGame
+//
+//  Created by delma on 2020/05/06.
+//  Copyright © 2020 delma. All rights reserved.
+//
+
+import UIKit
+
+class VersusLabel: UILabel {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.translatesAutoresizingMaskIntoConstraints = false
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    private func configure() {
+        self.font = UIFont.boldSystemFont(ofSize: 18)
+        self.textColor = .darkGray
+        self.text = "VS"
+    }
+   
+}
