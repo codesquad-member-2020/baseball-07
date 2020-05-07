004aeadb25a9d5fc1fe3c77e0eb6364a38cc0a33
koo bohyeon <goosee115@gmail.com>
2020-05-07 15:21:38 +0900

    feature:rocket: user 테이블 추가

--- BE/src/main/resources/schema.sql
+++ BE/src/main/resources/schema.sql
@@ -1,6 +1,13 @@
+CREATE TABLE user
+(
+    email varchar(256) primary key
+);
+
 CREATE TABLE game
 (
     id bigint auto_increment primary key,
+    home_team_user varchar(256) references user(email),
+    away_team_user varchar(256) references  user(email)
 );
 
 CREATE TABLE team
