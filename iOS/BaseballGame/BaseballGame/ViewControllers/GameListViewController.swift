de9a3fe6be2ec156d1472d1a5a92d7c1a7afcd70
delma <sdy2856@gmail.com>
2020-05-08 19:15:07 +0900

    [#5] feat :rocket: 커스텀 액티비티 인디케이터 생성 및 기능 구현

--- iOS/BaseballGame/BaseballGame/ViewControllers/GameListViewController.swift
+++ iOS/BaseballGame/BaseballGame/ViewControllers/GameListViewController.swift
@@ -18,17 +18,23 @@ final class GameListViewController: UIViewController {
     private let dispatcher = NetworkDispatcher()
     private lazy var task = NetworkTask(dispatcher: dispatcher)
     
+    private var networkIndicator: NetworkIndicator!
+  
     override func viewDidLoad() {
         super.viewDidLoad()
         configure()
+        networkIndicator.startAnimating()
         requestData()
     }
     
     private func configure() {
+        let width = view.frame.width / 8
         gameListTableView.register(GameListTableViewCell.self, forCellReuseIdentifier: "GameListTableViewCell")
+        networkIndicator = NetworkIndicator(frame: CGRect(x: view.center.x - width / 2, y: view.center.y - width / 2, width: width, height: width ), image: UIImage(named: "99.png"))
         
         self.view.addSubview(gameListTitle)
         self.view.addSubview(gameListTableView)
+        self.view.addSubview(networkIndicator)
         
         configureConstraints()
     }
@@ -57,6 +63,7 @@ final class GameListViewController: UIViewController {
                     self.gameListDataSource = GameListTableDataSource(gameList: decodedData as! GameList)
                     self.gameListTableView.dataSource = self.gameListDataSource
                     self.gameListTableView.reloadData()
+                    self.networkIndicator.stopAnimating()
                 }
             case .failure(let error):
                 print("\(error) 알럿 띄워쥬기")
@@ -66,4 +73,8 @@ final class GameListViewController: UIViewController {
     
 }
 
-
+extension GameListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //server에 해당 방 비었는지 요청 후 true / false 로 받아 true일때 알럿 띄워 홈/원정 선택 후 방 입장..
+    }
+}
