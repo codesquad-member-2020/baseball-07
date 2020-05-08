239178c6f772e5a928766ac08ac86f678782330a
koo bohyeon <goosee115@gmail.com>
2020-05-09 01:58:41 +0900

    feat :rocket: 게임 리스트 반환 api 컨트롤러에 추가함
    게임 요청에 응답하기 위해 게임 리스트 관련 controller 작성함
    service 클래스에서 반환 값을 가공하여 넘겨주도록 함

---
+++ BE/src/main/java/com/codesquad/baseball07/controller/GameController.java
@@ -0,0 +1,24 @@
+package com.codesquad.baseball07.controller;
+
+import com.codesquad.baseball07.dto.Game;
+import com.codesquad.baseball07.service.GameService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class GameController {
+
+    private final GameService gameService;
+
+    public GameController(GameService gameService) {
+        this.gameService = gameService;
+    }
+
+    @GetMapping("/games")
+    public Map<String, List<Game>> games() {
+        return gameService.getAll();
+    }
+}
