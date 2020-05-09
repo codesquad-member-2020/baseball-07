cbb5ec6d9819b3af47438028484b5a42c244f29f
delma <sdy2856@gmail.com>
2020-05-09 19:58:48 +0900

    [#6] feat :rocket: PlayViewController, ScoresViewController 생성 및 스토리보드와 연결

---
+++ iOS/BaseballGame/BaseballGame/ViewControllers/PlayViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PlayViewController.swift
+//  BaseballGame
+//
+//  Created by delma on 2020/05/09.
+//  Copyright © 2020 delma. All rights reserved.
+//
+
+import UIKit
+
+class PlayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ iOS/BaseballGame/BaseballGame/ViewControllers/ScoresViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ScoresViewController.swift
+//  BaseballGame
+//
+//  Created by delma on 2020/05/09.
+//  Copyright © 2020 delma. All rights reserved.
+//
+
+import UIKit
+
+class ScoresViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
