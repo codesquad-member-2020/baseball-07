470ea87e348f6f31df93e76b87de89cc0345b55c
sedin2 <53131108+sedin2@users.noreply.github.com>
2020-05-12 03:26:05 +0900

    feat :rocket: GameController에 게임 참가 가능한 리스트 반환 api 추가
    controller에 메소드 추가
    로직은 service에서 처리 하도록 만듦

--- BE/src/main/java/com/codesquad/baseball07/controller/GameController.java
+++ BE/src/main/java/com/codesquad/baseball07/controller/GameController.java
@@ -1,8 +1,10 @@
 package com.codesquad.baseball07.controller;
 
 import com.codesquad.baseball07.dto.Game;
+import com.codesquad.baseball07.dto.GameDto;
 import com.codesquad.baseball07.service.GameService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,5 +22,10 @@ public class GameController {
     @GetMapping("/games")
     public Map<String, List<Game>> games() {
         return gameService.getAll();
+    }
+
+    @GetMapping("/games/{gameId}")
+    public GameDto getGameForEntry(@PathVariable("gameId") Long gameId) {
+        return gameService.getGameForEntry(gameId);
     }
 }
