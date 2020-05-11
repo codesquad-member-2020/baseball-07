20cb63c4a480b297796772193e2b914bcfa31214
sedin2 <53131108+sedin2@users.noreply.github.com>
2020-05-11 17:03:00 +0900

    feat :rocket: ResponseData 추가
    ResponseData 추가

---
+++ BE/src/main/java/com/codesquad/baseball07/response/ResponseData.java
@@ -0,0 +1,14 @@
+package com.codesquad.baseball07.response;
+
+public class ResponseData {
+
+    private Object result;
+
+    public ResponseData(Object result) {
+        this.result = result;
+    }
+
+    public Object getResult() {
+        return result;
+    }
+}
