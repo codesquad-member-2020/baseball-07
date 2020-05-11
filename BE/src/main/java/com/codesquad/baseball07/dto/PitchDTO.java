89f975f96a4ae911adc51774f63cb4b88dc2bc2c
sedin2 <53131108+sedin2@users.noreply.github.com>
2020-05-11 17:02:22 +0900

    feat :rocket: PitchDTO추가
    PitchDTO 추가

---
+++ BE/src/main/java/com/codesquad/baseball07/dto/PitchDTO.java
@@ -0,0 +1,14 @@
+package com.codesquad.baseball07.dto;
+
+import com.codesquad.baseball07.entity.Player;
+
+public class PitchDTO {
+
+    private Player pitcher;
+    private Player hitter;
+    private Long inning;
+    private String turn;
+    private String pitch_result;
+    private boolean out;
+
+}
