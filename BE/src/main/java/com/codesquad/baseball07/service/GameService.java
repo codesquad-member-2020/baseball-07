b4bbfc4b21270b1bd0077813d1d70a829af894b4
sedin2 <53131108+sedin2@users.noreply.github.com>
2020-05-12 03:28:15 +0900

    feat :rocket: GameService에 게임 참가 가능한 리스트 반환 method 추가
    service에 메소드 추가
    game의 id를 담아 dao 호출

--- BE/src/main/java/com/codesquad/baseball07/service/GameService.java
+++ BE/src/main/java/com/codesquad/baseball07/service/GameService.java
@@ -2,6 +2,7 @@ package com.codesquad.baseball07.service;
 
 import com.codesquad.baseball07.dao.GameDao;
 import com.codesquad.baseball07.dto.Game;
+import com.codesquad.baseball07.dto.GameDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,9 +17,13 @@ public class GameService {
     GameDao gameDao;
 
     public Map<String, List<Game>> getAll() {
-        List<Game> games =  gameDao.getAll();
+        List<Game> games = gameDao.getAll();
         Map<String, List<Game>> map = new HashMap<>();
         map.put("games", games);
         return map;
+    }
+
+    public GameDto getGameForEntry(Long gameId) {
+        return gameDao.getGameForEntry(gameId);
     }
 }
