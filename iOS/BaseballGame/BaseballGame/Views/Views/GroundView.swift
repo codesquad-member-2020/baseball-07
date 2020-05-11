f7c2c106144b91f01edec16961aec0384ff3efe4
delma <sdy2856@gmail.com>
2020-05-11 15:35:19 +0900

    [#6] chore :sparkles: assets에 컬러칩 추가

---
+++ iOS/BaseballGame/BaseballGame/Views/Views/GroundView.swift
@@ -0,0 +1,21 @@
+//
+//  GroundView.swift
+//  BaseballGame
+//
+//  Created by delma on 2020/05/11.
+//  Copyright © 2020 delma. All rights reserved.
+//
+
+import UIKit
+
+class GroundView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
