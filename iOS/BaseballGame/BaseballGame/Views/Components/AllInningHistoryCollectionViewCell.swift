1453ee69b6106f0fd5fad30461db341c25fbc5b5
delma <sdy2856@gmail.com>
2020-05-12 19:36:00 +0900

    [#6] feat :rocket: AllInningHistoryCollectionViewCell 생성 및 해당 셀이 내부 테이블뷰의 데이터소스 채택하도록 함

---
+++ iOS/BaseballGame/BaseballGame/Views/Components/AllInningHistoryCollectionViewCell.swift
@@ -0,0 +1,64 @@
+//
+//  AllInningHistoryCollectionViewCell.swift
+//  BaseballGame
+//
+//  Created by delma on 2020/05/12.
+//  Copyright © 2020 delma. All rights reserved.
+//
+
+import UIKit
+
+class AllInningHistoryCollectionViewCell: UICollectionViewCell, UITableViewDataSource {
+
+    
+    static let identifier = "AllInningHistoryCollectionViewCell"
+    private let nowTurnPlayerInfoView = NowTurnPlayerInfoStackView()
+    private let playHistoryTableView = PlayHistoryTableView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    private func configure() {
+        playHistoryTableView.dataSource = self
+    }
+    
+    private func configureSubViews() {
+        self.addSubview(nowTurnPlayerInfoView)
+        self.addSubview(playHistoryTableView)
+        
+        configureConstraints()
+    }
+    
+    private func configureConstraints() {
+        let constraints = [
+            nowTurnPlayerInfoView.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: 4),
+            nowTurnPlayerInfoView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: 16),
+            nowTurnPlayerInfoView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: -16),
+            
+            playHistoryTableView.topAnchor.constraint(equalTo: nowTurnPlayerInfoView.bottomAnchor, constant: 4),
+            playHistoryTableView.leadingAnchor.constraint(equalTo: self.nowTurnPlayerInfoView.leadingAnchor),
+            playHistoryTableView.trailingAnchor.constraint(equalTo: nowTurnPlayerInfoView.trailingAnchor),
+            playHistoryTableView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor, constant: -4),
+        ]
+        constraints.forEach { $0.isActive = true }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // TODO: - 각각의 데이터 넣어줘야함.
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PlayHistoryTableViewCell", for: indexPath) as! PlayHistoryTableViewCell
+        
+        return cell
+    }
+    
+}
---
+++ iOS/BaseballGame/BaseballGame/Views/Views/AllInningHistoryCollectionView.swift
@@ -0,0 +1,23 @@
+//
+//  AllInningHistoryCollectionView.swift
+//  BaseballGame
+//
+//  Created by delma on 2020/05/12.
+//  Copyright © 2020 delma. All rights reserved.
+//
+
+import UIKit
+
+class AllInningHistoryCollectionView: UICollectionView {
+    
+    override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
+        super.init(frame: frame, collectionViewLayout: layout)
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.backgroundColor = .clear
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+}
