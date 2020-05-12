eee31be8fb0fd6e625a0f041b271866ce6d1e2ba
sedin2 <53131108+sedin2@users.noreply.github.com>
2020-05-12 19:59:41 +0900

    feature :rocket: pitch 버튼 클릭 시 service에 로직 추가
    pitch 버튼 클릭 시 service에 로직을 처리 하도록 추가

--- BE/src/main/java/com/codesquad/baseball07/service/GameService.java
+++ BE/src/main/java/com/codesquad/baseball07/service/GameService.java
@@ -2,6 +2,7 @@ package com.codesquad.baseball07.service;
 
 import com.codesquad.baseball07.dao.GameDao;
 import com.codesquad.baseball07.dto.EntryDto;
+import com.codesquad.baseball07.dto.ResultDto;
 import com.codesquad.baseball07.entity.Game;
 import com.codesquad.baseball07.dto.GameDto;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,5 +31,9 @@ public class GameService {
 
     public EntryDto enterGame(Long gameId, String teamName) {
         return gameDao.enterGame(gameId, teamName);
+    }
+
+    public ResultDto pitch(Long gameId, String teamName) {
+        return gameDao.createBall(gameId, teamName);
     }
 }
