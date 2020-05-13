b570a8dd920cdd5c39f72c7e7f2b83df6f2ab9b7
delma <sdy2856@gmail.com>
2020-05-13 21:10:55 +0900

    [#6] feat :rocket: inningCollectionViewCell 의 다른 셀 선택시 첫번째 셀 해제되도록 구현

--- iOS/BaseballGame/BaseballGame/Delegates/InningCollectionViewDelegate.swift
+++ iOS/BaseballGame/BaseballGame/Delegates/InningCollectionViewDelegate.swift
@@ -13,6 +13,10 @@ class InningCollectionViewDelegate: NSObject, UICollectionViewDelegate {
         let cell = collectionView.cellForItem(at: indexPath) as! InningCollectionViewCell
         cell.selected()
         NotificationCenter.default.post(name: .selectInningCell, object: nil, userInfo: ["indexPath":indexPath])
+        if indexPath.row != 0 {
+            guard let firstCell = collectionView.cellForItem(at: [0,0]) as? InningCollectionViewCell else { return }
+            firstCell.deselected()
+        }
         // TODO: - 하단에 뷰 생성해서 걔한테 [1회] 셀이 선택되었다는 거 알려줘야 함
     }
     
