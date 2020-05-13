a74dd23a7c2ff1ac209638afca353733e641402b
koo bohyeon <goosee115@gmail.com>
2020-05-13 16:13:18 +0900

    fix :pushpin: 쿠키 설정 작업 되돌림
    잘 모르고 쓴 코드 삭제함,쿠키 설정이 안되서 삭제해봄

--- BE/src/main/java/com/codesquad/baseball07/controller/LoginController.java
+++ BE/src/main/java/com/codesquad/baseball07/controller/LoginController.java
@@ -1,13 +1,13 @@
 package com.codesquad.baseball07.controller;
 
 import com.codesquad.baseball07.service.LoginService;
-import org.springframework.http.ResponseCookie;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 
 @RestController
@@ -31,8 +31,8 @@ public class LoginController {
     @GetMapping("/login")
     public RedirectView login(@RequestParam String code, HttpServletResponse httpServletResponse) {
         String jws = this.loginService.authenticate(code);
-        ResponseCookie responseCookie = ResponseCookie.from(loginService.getCookieName(), jws).domain("13.125.173.210").sameSite("None").secure(true).path("/").build();
-        httpServletResponse.addHeader("Set-Cookie", responseCookie.toString());
+        Cookie cookie = new Cookie(loginService.getCookieName(), jws);
+        httpServletResponse.addCookie(cookie);
 
         RedirectView redirectView = new RedirectView();
         redirectView.setUrl(loginService.getFrontMainUri());
