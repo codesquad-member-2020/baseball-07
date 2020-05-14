b3319a87e2a904e1287b737f258bd01d09d7cfd8
koo bohyeon <goosee115@gmail.com>
2020-05-14 20:58:37 +0900

    feature: rocket: lombok설정이 안되서 일단 생성자 추가함

--- BE/src/main/java/com/codesquad/baseball07/entity/PitchingRecord.java
+++ BE/src/main/java/com/codesquad/baseball07/entity/PitchingRecord.java
@@ -27,4 +27,23 @@ public class PitchingRecord {
         this.ball = ballId;
     }
 
+    public PitchingRecord(Long ball, Integer inning, String turn, Integer ball_count, Integer turn_at_bat_count, Integer hit_count, Boolean strike_out) {
+        this.ball = ball;
+        this.inning = inning;
+        this.turn =turn;
+        this.ballCount = ball_count;
+        this.turnAtBatCount =turn_at_bat_count;
+        this.hitCount = hit_count;
+        this.strikeOut = strike_out;
+    }
+
+    public PitchingRecord(long ball, int inning, String turn, int ball_count, int turn_at_bat_count, int hit_count, boolean strike_out) {
+        this.ball = ball;
+        this.inning = inning;
+        this.turn =turn;
+        this.ballCount = ball_count;
+        this.turnAtBatCount =turn_at_bat_count;
+        this.hitCount = hit_count;
+        this.strikeOut = strike_out;
+    }
 }
