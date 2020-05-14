e0ebc400bccc7860d9938e5b2de9ad55235b27b3
sedin2 <53131108+sedin2@users.noreply.github.com>
2020-05-14 18:06:58 +0900

    feature :rocket: 이닝 별 타자 기록 api 추가
    controller에 추가

--- BE/src/main/java/com/codesquad/baseball07/controller/GameController.java
+++ BE/src/main/java/com/codesquad/baseball07/controller/GameController.java
@@ -3,7 +3,8 @@ package com.codesquad.baseball07.controller;
 import com.codesquad.baseball07.dto.EntryDto;
 import com.codesquad.baseball07.dto.GameDto;
 import com.codesquad.baseball07.entity.Game;
-import com.codesquad.baseball07.response.ResponseData;
+import com.codesquad.baseball07.response.ResponseHitterData;
+import com.codesquad.baseball07.response.ResponsePitchData;
 import com.codesquad.baseball07.service.GameService;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -50,5 +51,12 @@ public class GameController {
                 gameService.pitch(gameId, teamName)), HttpStatus.OK);
     }
 
+    @GetMapping("/games/{gameId}/teams/{teamName}/inning/{inning}")
+    public ResponseEntity<ResponseHitterData> getHitterHistory(@PathVariable("gameId") Long gameId,
+                                                               @PathVariable("teamName") String teamName,
+                                                               @PathVariable("inning") int inning) {
+
+        return new ResponseEntity<>(new ResponseHitterData(
+                gameService.getHitterHistory(gameId, teamName, inning)), HttpStatus.OK);
     }
 }
