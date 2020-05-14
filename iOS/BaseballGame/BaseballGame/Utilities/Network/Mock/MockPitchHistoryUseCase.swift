405bc1d7e44e6b9d363e16693d72cf0223c2bc7c
delma <sdy2856@gmail.com>
2020-05-14 20:12:19 +0900

    [#7] feat :rocket: MockPitchHistoryUseCase 생성

--- iOS/BaseballGame/BaseballGame/Utilities/Network/Mock/MockPitchHistoryUseCase.swift
+++ iOS/BaseballGame/BaseballGame/Utilities/Network/Mock/MockPitchHistoryUseCase.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+
+struct MockInningHistoryUseCase {
+    private let task = NetworkTask(dispatcher: NetworkDispatcher())
+    
+    class MockPitchRequest: Request {
+        var path: String = EndPoints.fakePlayInfo
+    }
+    
+    func requestPitchStub(handler: @escaping (Any) -> Void) {
+        task.perform(request: MockPitchRequest(), dataType: InningHistory.self) { result in
+            switch result {
+            case .success(let decodedData):
+                handler(decodedData)
+            case .failure(let error):
+                NotificationCenter.default.post(name: .networkError, object: nil, userInfo: ["error":error])
+            }
+        }
+    }
+}
