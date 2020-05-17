589dd7e14bc1e18c02d0a2e213554dc6a2074559
delma <sdy2856@gmail.com>
2020-05-17 20:07:33 +0900

    [#7] feat :rocket: URL에 사용되는 기본 String 값 추가

--- iOS/BaseballGame/BaseballGame/Utilities/Network/EndPoints.swift
+++ iOS/BaseballGame/BaseballGame/Utilities/Network/EndPoints.swift
@@ -11,6 +11,8 @@ import Foundation
 enum EndPoints {
     static let defaultURL = "http://52.78.108.151:8080/"
     static let games = "games"
+    static let teams = "teams"
+    static let inning = "inning"
     static let fakeGameList = "http://www.mocky.io/v2/5ebe2ff03100006800c5d048"
     static let fakeGameRoomEmpty = "http://www.mocky.io/v2/5eb6705a3100006900699a0c"
     static let fakePitch = "http://www.mocky.io/v2/5ebcedc731000059005b0e39"
